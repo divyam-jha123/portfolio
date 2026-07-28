741486223ab8e44fa94a059c0556f28e50d5f899
divyamjha <divyamjha.70055594@gmail.com>
2026-07-28 21:25:01 +0530

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeToggle } from './components/ThemeToggle';
 import { Dock } from './components/Dock';
 import { HeroSection } from './components/HeroSection';
 import { ProjectSection } from './components/ProjectSection';
-import { InBetweenSection } from './components/InBetweenSection';
 import { ExperienceSection } from './components/ExperienceSection';
 import { EducationSection } from './components/EducationSection';
 import { TechStackSection } from './components/TechStackSection';
